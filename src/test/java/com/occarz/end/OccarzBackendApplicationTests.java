69a142c1400066aee3ee233dc27752406bd37151
SOLOFOARISON Tolotry Ny Avo Arisaina <62713580+Aris1401@users.noreply.github.com>
2024-01-08 21:02:00 +0300

    initial

---
+++ src/test/java/com/occarz/end/OccarzBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.occarz.end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OccarzBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
