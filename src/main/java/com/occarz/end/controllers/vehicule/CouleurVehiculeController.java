8a58f3bed9f34296f1d7d6ad09822ab333eeb556
SOLOFOARISON Tolotry Ny Avo Arisaina <62713580+Aris1401@users.noreply.github.com>
2024-01-29 10:12:17 +0300

    couleur vehicule crud

---
+++ src/main/java/com/occarz/end/controllers/vehicule/CouleurVehiculeController.java
@@ -0,0 +1,44 @@
+package com.occarz.end.controllers.vehicule;
+
+import com.occarz.end.dto.response.RestResponse;
+import com.occarz.end.entities.vehicule.BoiteDeVitesse;
+import com.occarz.end.entities.vehicule.CouleurVehicule;
+import com.occarz.end.repository.voiture.BoiteDeVitesseRepository;
+import com.occarz.end.repository.voiture.CouleurVehiculeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/admin/couleurVehicules")
+@CrossOrigin(origins = "*")
+public class CouleurVehiculeController {
+    @Autowired
+    CouleurVehiculeRepository couleurVehiculeRepository;
+
+    @GetMapping("")
+    public RestResponse<List<CouleurVehicule>> allMarques() {
+        return new RestResponse<>(couleurVehiculeRepository.findAll());
+    }
+
+    @GetMapping("/{idMarque}")
+    public RestResponse<CouleurVehicule> marqueById(@PathVariable("idMarque") int idMarque) {
+        return new RestResponse<>(couleurVehiculeRepository.findById(idMarque).get());
+    }
+
+    @DeleteMapping("/{idMarque}")
+    public void deleteMarque(@PathVariable("idMarque") int idMarque) {
+        couleurVehiculeRepository.deleteById(idMarque);
+    }
+
+    @PutMapping("")
+    public RestResponse<CouleurVehicule> updateMarque(@RequestBody CouleurVehicule marque) {
+        return new RestResponse<>(couleurVehiculeRepository.save(marque));
+    }
+
+    @PostMapping("")
+    public RestResponse<CouleurVehicule> saveMarque(@RequestBody CouleurVehicule marque) {
+        return new RestResponse<>(couleurVehiculeRepository.save(marque));
+    }
+}
